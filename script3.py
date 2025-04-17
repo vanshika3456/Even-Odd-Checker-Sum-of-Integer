d7fc03ed1e8d38505808c9c26b3b857634157f7d
VANSHIKA JAITIK <jaitikvanshika@gmail.com>
2025-04-17 22:28:49 +0530

    Add files via upload

---
+++ script3.py
@@ -0,0 +1,8 @@
+#task 1
+#Check if a Number is Even or Odd
+#Takes an integer input from the user.
+n= int(input("Enter the number:"))
+if(n%2==0):#Checks whether the number is even or odd
+    print(n,"is an even no")
+else:
+    print(n,"is an odd no")
---
+++ script4.py
@@ -0,0 +1,10 @@
+#task 2
+# Sum of Integers from 1 to 50 Using a Loop
+# Initialize sum variable
+sum=0
+
+# Iterate from 1 to 50
+for i in range(1,51):
+    sum=sum+i
+# Print the result
+print("The sum of numbers from 1 to 50 is:", sum)
